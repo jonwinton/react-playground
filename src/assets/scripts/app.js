bb187ffb1f2915e127efeade26045f95ed92ca1f
Jon Winton <winton.jon@gmail.com>
2015-08-31 17:48:52 -0400

    Added sample component and initialization of 'app'

---
+++ src/assets/scripts/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
+
+React.render(<HelloWorld />, document.body);
---
+++ src/assets/scripts/components/HelloWorld.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>Hello, World!</div>
+        )
+    }
+}
+
+module.exports = HelloWorld;
